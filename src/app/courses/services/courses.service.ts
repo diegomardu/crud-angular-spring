cad58ba80fff6408aa124660590a397c6c5f76e1
Diego Martins Duarte <diegomartins836@gmail.com>
2021-11-02 19:18:01 -0300

    Chamadas http get

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,18 +1,21 @@
 import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = "/assets/courses.json"
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[]{
-
-    return [{
-      _id:'1', name: 'Angular', category: 'Front-end'
-    },{_id:'2', name: 'Java - Springboot', category: 'Back-end'}];
+  list(){
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(courses => console.log(courses))
+    );
   }
 }
